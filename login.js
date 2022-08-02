1436e4e5f1caa33b931fef2f64a704b00c4aeb56
awasthi2001 <akshanshawasthi013@gmail.com>
2022-08-02 09:22:53 +0530

    Adding login Page

--- login.js
+++ login.js
@@ -9,3 +9,29 @@ document.getElementById("container").style.display="flex";
 document.getElementById("parentLogin").style.display="flex";
 document.getElementById("childlogin").style.display="flex";
 }
+
+document.querySelector("#LoginId").addEventListener("submit",setData);
+var loginInfo = JSON.parse(localStorage.getItem("SignUpInfo")) || [];
+function setData(){
+event.preventDefault();
+var Mobileno = document.querySelector(".form-textbox").value;
+var LoginPass = document.querySelector("LoginPassword").value;
+
+var flag = false;
+   if(loginInfo==[]){
+    alert("Sign up first");
+   }else{
+    loginInfo.forEach(function(element){
+        if(element.MobileNumber==Mobileno && element.Password==LoginPass){
+    flag = true;
+   }
+})
+   }
+   if(flag==true){
+    alert("Sign in Succesfull");
+    window.open("index.html","_self")
+   }else{
+    alert("wrong Credentials")
+   }
+}
+
