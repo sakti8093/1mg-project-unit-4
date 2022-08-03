f58be3761708e5f6daf24c8d5514b622ea2cc3d3
awasthi2001 <akshanshawasthi013@gmail.com>
2022-08-03 14:48:33 +0530

    few changes in login and signUp page

--- login.js
+++ login.js
@@ -1,10 +1,10 @@
 function closePopUp(){
-    document.getElementById("container").style.display="none";
+    document.getElementById("Logincontainer").style.display="none";
     document.getElementById("parentLogin").style.display="none";
     document.getElementById("childlogin").style.display="none";
 }
 function loginPopUp(){
-document.getElementById("container").style.display="flex";
+document.getElementById("Logincontainer").style.display="flex";
 document.getElementById("parentLogin").style.display="flex";
 document.getElementById("childlogin").style.display="flex";
 }
